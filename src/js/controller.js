0969ddb4e050a4bda82105bee39b3e911ca7ef74
Manuel De Marco <manueldm769@gmail.com>
2026-05-31 11:37:50 +0200

    Polyfilling , rendering ingredients loopin over ing array , rendering spinner before fetching

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,3 +1,11 @@
+// import icons from '../img/icons.svg'; //Parcel 1
+import icons from 'url:../img/icons.svg'; //PArcel 2
+
+//Tool di polyfill , core-js inserisce metodi e funzionalita es6 in old browsers
+import 'core-js/stable'; //Importo solo la versione stable
+//Regenerator runtime fa funzionare async await
+import 'regenerator-runtime/runtime'; //Importo solo il runtime
+
 const recipeContainer = document.querySelector('.recipe');
 
 const timeout = function (s) {
@@ -13,12 +21,26 @@ const timeout = function (s) {
 
 ///////////////////////////////////////
 
+const renderSpinner = function (parentEl) {
+  const markup = ` 
+       <div class ="spinner">
+          <svg>
+            <use href="${icons}#icon-loader"></use>
+          </svg>
+       </div>
+          `;
+
+  parentEl.innerHTML = '';
+  parentEl.insertAdjacentHTML('afterbegin', markup);
+};
 const showRecipe = async function () {
   try {
     //Loading recipe
+
+    //Per lo spinner semplicemente lo inserisco prima del fetch . Poi il contenuto del fetch sostituirà lo spinner
+    renderSpinner(recipeContainer);
     const res = await fetch(
-      // `https://forkify-api.jonas.io/api/v2/recipes/5ed6604591c37cdc054bc886?`
-      `https://forkify-api.jonas.io/api/v2/recipes/664c8f193e7aa067e94e85be?`,
+      `https://forkify-api.jonas.io/api/v2/recipes/5ed6604591c37cdc054bc886?`,
     );
     const data = await res.json();
     console.log(res, data);
@@ -51,14 +73,14 @@ const showRecipe = async function () {
         <div class="recipe__details">
           <div class="recipe__info">
             <svg class="recipe__info-icon">
-              <use href="src/img/icons.svg#icon-clock"></use>
+              <use href="${icons}#icon-clock"></use>
             </svg>
             <span class="recipe__info-data recipe__info-data--minutes">${recipe.cookingTime}</span>
             <span class="recipe__info-text">minutes</span>
           </div>
           <div class="recipe__info">
             <svg class="recipe__info-icon">
-              <use href="src/img/icons.svg#icon-users"></use>
+              <use href="${icons}#icon-users"></use>
             </svg>
             <span class="recipe__info-data recipe__info-data--people">${recipe.servings}</span>
             <span class="recipe__info-text">servings</span>
@@ -66,12 +88,12 @@ const showRecipe = async function () {
             <div class="recipe__info-buttons">
               <button class="btn--tiny btn--increase-servings">
                 <svg>
-                  <use href="src/img/icons.svg#icon-minus-circle"></use>
+                  <use href="${icons}#icon-minus-circle"></use>
                 </svg>
               </button>
               <button class="btn--tiny btn--increase-servings">
                 <svg>
-                  <use href="src/img/icons.svg#icon-plus-circle"></use>
+                  <use href="${icons}#icon-plus-circle"></use>
                 </svg>
               </button>
             </div>
@@ -79,40 +101,38 @@ const showRecipe = async function () {
 
           <div class="recipe__user-generated">
             <svg>
-              <use href="src/img/icons.svg#icon-user"></use>
+              <use href="${icons}#icon-user"></use>
             </svg>
           </div>
           <button class="btn--round">
             <svg class="">
-              <use href="src/img/icons.svg#icon-bookmark-fill"></use>
+              <use href="${icons}#icon-bookmark-fill"></use>
             </svg>
           </button>
         </div>
-
         <div class="recipe__ingredients">
           <h2 class="heading--2">Recipe ingredients</h2>
           <ul class="recipe__ingredient-list">
+          ${
+            recipe.ingredients
+              .map(ing => {
+                //Ciclo sull array e ritorno una stringa di html che poi inseriro come adjacent
+                return `
             <li class="recipe__ingredient">
               <svg class="recipe__icon">
-                <use href="src/img/icons.svg#icon-check"></use>
+                <use href="${icons}#icon-check"></use>
               </svg>
-              <div class="recipe__quantity">1000</div>
+              <div class="recipe__quantity">${ing.quantity}</div>
               <div class="recipe__description">
-                <span class="recipe__unit">g</span>
-                pasta
+                <span class="recipe__unit">${ing.unit}</span>
+                ${ing.description}
               </div>
             </li>
+            `;
+              })
+              .join('') //Transformato in stringa
+          }
 
-            <li class="recipe__ingredient">
-              <svg class="recipe__icon">
-                <use href="src/img/icons.svg#icon-check"></use>
-              </svg>
-              <div class="recipe__quantity">0.5</div>
-              <div class="recipe__description">
-                <span class="recipe__unit">cup</span>
-                ricotta cheese
-              </div>
-            </li>
           </ul>
         </div>
 
@@ -132,10 +152,13 @@ const showRecipe = async function () {
           >
             <span>Directions</span>
             <svg class="search__icon">
-              <use href="src/img/icons.svg#icon-arrow-right"></use>
+              <use href="${icons}#icon-arrow-right"></use>
             </svg>
           </a>
         </div>`;
+
+    //Rimuovi html placeholder
+    recipeContainer.innerHTML = '';
 
     recipeContainer.insertAdjacentHTML('afterbegin', markup);
   } catch (err) {
