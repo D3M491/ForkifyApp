50e796ad417329f34d6ae69a75cd7869991a2292
Manuel De Marco <manueldm769@gmail.com>
2026-06-02 17:10:06 +0200

    big

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,12 +1,11 @@
-// import icons from '../img/icons.svg'; //Parcel 1
-import icons from 'url:../img/icons.svg'; //PArcel 2
+//Import tutto dal model
+import * as model from './model.js';
+import recipeView from './views/recipeView';
 
 //Tool di polyfill , core-js inserisce metodi e funzionalita es6 in old browsers
 import 'core-js/stable'; //Importo solo la versione stable
 //Regenerator runtime fa funzionare async await
 import 'regenerator-runtime/runtime'; //Importo solo il runtime7s
-
-const recipeContainer = document.querySelector('.recipe');
 
 const timeout = function (s) {
   return new Promise(function (_, reject) {
@@ -21,149 +20,30 @@ const timeout = function (s) {
 
 ///////////////////////////////////////
 
-const renderSpinner = function (parentEl) {
-  const markup = ` 
-       <div class ="spinner">
-          <svg>
-            <use href="${icons}#icon-loader"></use>
-          </svg>
-       </div>
-          `;
-
-  parentEl.innerHTML = '';
-  parentEl.insertAdjacentHTML('afterbegin', markup);
-};
-const showRecipe = async function () {
+const controlRecipes = async function () {
   try {
-    //Loading recipe
+    //Ottengo l'url della barra di ricerca così
+    const id = window.location.hash.slice(1);
+    console.log(id);
 
+    if (!id) return;
     //Per lo spinner semplicemente lo inserisco prima del fetch . Poi il contenuto del fetch sostituirà lo spinner
-    renderSpinner(recipeContainer);
-    const res = await fetch(
-      `https://forkify-api.jonas.io/api/v2/recipes/5ed6604591c37cdc054bc886?`,
-    );
-    const data = await res.json();
-    console.log(res, data);
+    recipeView.renderSpinner();
 
-    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
-    //creating copy of object
-    let { recipe } = data.data;
-
-    //Renamed object properties
-    recipe = {
-      id: recipe.id,
-      title: recipe.title,
-      publisher: recipe.publisher,
-      sourceUrl: recipe.sourceUrl,
-      image: recipe.image_url,
-      servings: recipe.servings,
-      cookingTime: recipe.cooking_time,
-      ingredients: recipe.ingredients,
-    };
+    //Loading recipe
+    await model.loadRecipe(id);
+    console.log(model.state);
+    const { recipe } = model.state;
     console.log(recipe);
 
-    //rendering recipe
-    const markup = `<figure class="recipe__fig">
-          <img src="${recipe.image}" alt="Tomato" class="recipe__img" />
-          <h1 class="recipe__title">
-            <span>${recipe.title}</span>
-          </h1>
-        </figure>
-
-        <div class="recipe__details">
-          <div class="recipe__info">
-            <svg class="recipe__info-icon">
-              <use href="${icons}#icon-clock"></use>
-            </svg>
-            <span class="recipe__info-data recipe__info-data--minutes">${recipe.cookingTime}</span>
-            <span class="recipe__info-text">minutes</span>
-          </div>
-          <div class="recipe__info">
-            <svg class="recipe__info-icon">
-              <use href="${icons}#icon-users"></use>
-            </svg>
-            <span class="recipe__info-data recipe__info-data--people">${recipe.servings}</span>
-            <span class="recipe__info-text">servings</span>
-
-            <div class="recipe__info-buttons">
-              <button class="btn--tiny btn--increase-servings">
-                <svg>
-                  <use href="${icons}#icon-minus-circle"></use>
-                </svg>
-              </button>
-              <button class="btn--tiny btn--increase-servings">
-                <svg>
-                  <use href="${icons}#icon-plus-circle"></use>
-                </svg>
-              </button>
-            </div>
-          </div>
-
-          <div class="recipe__user-generated">
-            <svg>
-              <use href="${icons}#icon-user"></use>
-            </svg>
-          </div>
-          <button class="btn--round">
-            <svg class="">
-              <use href="${icons}#icon-bookmark-fill"></use>
-            </svg>
-          </button>
-        </div>
-        <div class="recipe__ingredients">
-          <h2 class="heading--2">Recipe ingredients</h2>
-          <ul class="recipe__ingredient-list">
-          ${
-            recipe.ingredients
-              .map(ing => {
-                //Ciclo sull array e ritorno una stringa di html che poi inseriro come adjacent
-                return `
-            <li class="recipe__ingredient">
-              <svg class="recipe__icon">
-                <use href="${icons}#icon-check"></use>
-              </svg>
-              <div class="recipe__quantity">${ing.quantity}</div>
-              <div class="recipe__description">
-                <span class="recipe__unit">${ing.unit}</span>
-                ${ing.description}
-              </div>
-            </li>
-            `;
-              })
-              .join('') //Transformato in stringa
-          }
-
-          </ul>
-        </div>
-
-        <div class="recipe__directions">
-          <h2 class="heading--2">How to cook it</h2>
-          <p class="recipe__directions-text">
-            This recipe was carefully designed and tested by
-            <span class="recipe__publisher">${
-              recipe.publisher
-            }</span>. Please check out
-            directions at their website.
-          </p>
-          <a
-            class="btn--small recipe__btn"
-            href="${recipe.sourceUrl}"
-            target="_blank"
-          >
-            <span>Directions</span>
-            <svg class="search__icon">
-              <use href="${icons}#icon-arrow-right"></use>
-            </svg>
-          </a>
-        </div>`;
-
-    //Rimuovi html placeholder
-    recipeContainer.innerHTML = '';
-
-    recipeContainer.insertAdjacentHTML('afterbegin', markup);
+    //Chiama il metodo render della classe recipeView passando la ricetta
+    recipeView.render(model.state.recipe);
   } catch (err) {
     alert(err);
   }
 };
 
-showRecipe();
+controlRecipes();
+
+//Ascoltando il cambio di hash nella barra di ricerca e il load , poi chiamo la funzione
+['hashchange', 'load'].forEach(e => window.addEventListener(e, controlRecipes));
