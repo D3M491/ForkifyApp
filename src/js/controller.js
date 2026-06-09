12c705337226ae02be1a8f4b46f08b80f0b401a4
Manuel De Marco <manueldm769@gmail.com>
2026-06-09 15:37:26 +0200

    Cleaned up controller

--- src/js/controller.js
+++ src/js/controller.js
@@ -138,10 +138,6 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
-const newFeature = function () {
-  console.log('Welcome to newfeature branch');
-};
-
 //Calling the handler function in the view passing my subscriber functions
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
@@ -151,7 +147,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  newFeature();
 };
 
 init();
